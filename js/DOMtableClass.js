a2dce7d5faace256c612773fc0ba505e84d4999c
TadasKundelis <kundelist@gmail.com>
2018-01-08 01:20:31 +0200

    Update DOMtableClass.js

--- js/DOMtableClass.js
+++ js/DOMtableClass.js
@@ -7,25 +7,25 @@ Each input field has a unique ID and the necessary event listeners attached to i
 */
 
 export default class DOMtable{
-	constructor() {
-		this.inputFields = []; //array for storing DOM div nodes that are inside table td nodes
-		this.currentField; // current div that has focus
-		this.previousField; 
-		this.validator; //reference/connection to the validator class that checks user's entered values
-	}
+  constructor() {
+    this.inputFields = []; //array for storing DOM div nodes that are inside table td nodes
+    this.currentField; // current div that has focus
+    this.previousField; 
+    this.validator; //reference/connection to the validator class that checks user's entered values
+  }
     
-	create(){
+  create(){
     for (let i = 0; i < 9; i++) {
-  	  this.inputFields[i] = [];
-        for (let j = 0; j < 9; j++) {
-          let current = divFields[i * 9 + j];
-          current.id = `${i}-${j}` //element id is created using the format "row-col" (ex. 0-0) to make it unique
-          current.addEventListener('keydown', this.handleKeyboardEvent.bind(this))
-          current.addEventListener('mousedown', this.highlightCurrentField.bind(this))
-          this.inputFields[i][j] = current
-        }
+      this.inputFields[i] = [];
+      for (let j = 0; j < 9; j++) {
+        let current = divFields[i * 9 + j];
+        current.id = `${i}-${j}` //element id is created using the format "row-col" (ex. 0-0) to make it unique
+        current.addEventListener('keydown', this.handleKeyboardEvent.bind(this))
+        current.addEventListener('mousedown', this.highlightCurrentField.bind(this))
+        this.inputFields[i][j] = current
       }
-	  }
+    }
+  }
 
   display(board) {
     for (let i = 0; i < 9; i++) {
@@ -50,80 +50,80 @@ export default class DOMtable{
 
   handleKeyboardEvent(event){
     event = event || window.event; 
-	  let keyValue = event.key;
-	  let id = this.currentField.id;
+    let keyValue = event.key;
+    let id = this.currentField.id;
     let [row, col] = id.split('-'); // id format is "row-col", we can get the coordinates by removing the hyphen 
-	  if (/^(Arrow)*(Up|Down|Right|Left)$/.test(keyValue)) {
+    if (/^(Arrow)*(Up|Down|Right|Left)$/.test(keyValue)) {
       event.preventDefault();
-	  	this.highlightArrowMovement(keyValue, row, col) 
-	  } else if (keyValue === 'Backspace' || keyValue === 'Delete') {
-	  	event.preventDefault(); // this is needed to prevent IE and mozzila from returning to a previous page when user hits backspace
-        if (!this.currentField.hasAttribute('readonly')) {
-	  	  this.validator.updateBoard(row, col, this.currentField.innerHTML, '')
-	  	  this.currentField.innerHTML = '';
-	  	} else { // if the field is readonly 
+      this.highlightArrowMovement(keyValue, row, col) 
+    } else if (keyValue === 'Backspace' || keyValue === 'Delete') {
+      event.preventDefault(); // this is needed to prevent IE and mozzila from returning to a previous page when user hits backspace
+      if (!this.currentField.hasAttribute('readonly')) {
+        this.validator.updateBoard(row, col, this.currentField.innerHTML, '')
+	this.currentField.innerHTML = '';
+      } else { // if the field is readonly 
         return
-	  	}
-	  } else if (/[1-9]/.test(keyValue)) { //numbers between [1-9]
-	  	if (!this.currentField.hasAttribute('readonly')) { //update the value if the field is not readonly
-	  	  let previousValue = this.currentField.innerHTML;
+      }
+    } else if (/[1-9]/.test(keyValue)) { //numbers between [1-9]
+      if (!this.currentField.hasAttribute('readonly')) { //update the value if the field is not readonly
+        let previousValue = this.currentField.innerHTML;
       	this.currentField.innerHTML = keyValue;
       	this.validator.updateBoard(row, col, previousValue, keyValue)
-      	} else {
-      	  return
-      	}
-      } else { // if any other key is pressed, prevent default behaviour
-      	return event.preventDefault();
+      } else {
+      	return
       }
+    } else { // if any other key is pressed, prevent default behaviour
+      return event.preventDefault();
+    }
+  }
+
+  highlightCurrentField(event, fieldToHighlight){
+    if (this.currentField) { //if there is already a current field
+      this.previousField = this.currentField; //make it previous field
+      this.previousField.classList.remove('current') 
+      //event.target is provided by mousedown event, if it's absent, we use second parameter provided by highlightArrowMovement function
+      this.currentField = event ? event.target : fieldToHighlight; 
+      } else { //if there's no current field, make event target current field
+        this.currentField = event.target;
+      }
+      this.currentField.classList.add('current')
     }
 
-    highlightCurrentField(event, fieldToHighlight){
-	  if (this.currentField) { //if there is already a current field
-	    this.previousField = this.currentField; //make it previous field
-		  this.previousField.classList.remove('current') 
-		  //event.target is provided by mousedown event, if it's absent, we use second parameter provided by highlightArrowMovement function
-		  this.currentField = event ? event.target : fieldToHighlight; 
-	  } else { //if there's no current field, make event target current field
-		  this.currentField = event.target;
-    }
-	  this.currentField.classList.add('current')
-	}
-
-	highlightArrowMovement(keyValue, row, col){
-      let fieldToHighlight;
-      keyValue = keyValue.replace('Arrow', '');
-	    switch(keyValue) {
-          case 'Left':
-            if (col > 0) {
-              --col
-              break;
-            } else {
-              return
-            }
-          case 'Up':
-            if (row > 0) {
-              --row
-              break;
-            } else {
-              return
-            }
-          case 'Right':
-            if (col < 8) {
-              ++col
-              break;
-            } else {
-              return
-            }
-          case 'Down':
-            if (row < 8) {
-              ++row
-              break;
-            } else {
-              return
-            }
+  highlightArrowMovement(keyValue, row, col){
+    let fieldToHighlight;
+    keyValue = keyValue.replace('Arrow', '');
+      switch(keyValue) {
+        case 'Left':
+          if (col > 0) {
+            --col
+            break;
+          } else {
+            return
+          }
+        case 'Up':
+          if (row > 0) {
+            --row
+            break;
+          } else {
+            return
+          }
+        case 'Right':
+          if (col < 8) {
+            ++col
+            break;
+          } else {
+            return
+          }
+        case 'Down':
+          if (row < 8) {
+            ++row
+            break;
+          } else {
+            return
+          }
     	}
-        fieldToHighlight = this.inputFields[row][col]
-        this.highlightCurrentField(null, fieldToHighlight)
+      fieldToHighlight = this.inputFields[row][col]
+      this.highlightCurrentField(null, fieldToHighlight)
     }
 
     highlightError(errorList) {
